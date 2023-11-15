5d9dcf0460a1b1bd798c9d6859a10e6404286425
keeganesser <keeganesser03@gmail.com>
2023-11-15 15:39:37 -0800

    Post Processing
    added post processing as well as updating the swing sound

--- Assets/scripts/PlayerSwing.cs
+++ Assets/scripts/PlayerSwing.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSound : MonoBehaviour
+public class PlayerSwing : MonoBehaviour
 {
     public AudioClip[] swingSounds;
 
